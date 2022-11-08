cbadb9d5d6aa2cfd4a6f250dd6728405fbb9d0d3
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2022-11-08 17:10:14 +0200

    Add files via upload

---
+++ E02. Exercise Basic Syntax, Conditional Statements and Loops/P01. Ages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ages
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int determinativeAge = int.Parse(Console.ReadLine());
+            if (determinativeAge >= 0 && determinativeAge <= 2 )
+            {
+                Console.WriteLine("baby");
+            }
+            else if (determinativeAge >= 3 && determinativeAge <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (determinativeAge >= 14 && determinativeAge <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (determinativeAge >= 20 && determinativeAge <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (determinativeAge >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+            Console.Read();
+        }
+    }
+}
